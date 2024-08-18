f30b593eed4f982078e767da72ae7efaa392ba86
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-08-18 20:32:14 +0900

    Update Mission_06.java

--- src/JavaMission/Mission_06.java
+++ src/JavaMission/Mission_06.java
@@ -1,19 +1,29 @@
 package JavaMission;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Mission_06 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println( Math.random() ); 
-		
-		System.out.println( random.nextInt(10) );
-		
-	
-		
-	}
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("몇 게임?");
+
+		int n = sc.nextInt();
+
+		int arr[] = new int[n];
+
+		for (int i = 1; i <= arr.length; i++) {
+			System.out.print("[" + i + "게임] : ");
+			for (int j = 0; j < 6; j++) {
+				System.out.print((int) (Math.random() * 45) + 1 + " ");
+			}
+			System.out.println();
+		}
+
+
+	}
 }
+
