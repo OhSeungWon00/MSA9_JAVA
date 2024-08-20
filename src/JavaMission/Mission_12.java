b038fdd65af27c4b9105d7337eeb16bad70ebf32
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-08-20 14:28:58 +0900

    Add files via upload

---
+++ src/JavaMission/Mission_12.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.Scanner;
+
+public class Mission_12 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("몇 게임? ");
+
+		int arr[] = new int[6];
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("[" + i + "게임" + "] :");
+			for (int j = 0; j < 6; j++) {
+				arr[j] = (int) (Math.random() * 45 + 1);
+				
+				System.out.print(arr[j] + " ");
+
+			}
+			System.out.println();
+
+		}
+
+		}
+	}
+
